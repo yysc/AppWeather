23b1f6179716cc0717506dcbc586d9b4279f6e73
yysc <494792342@qq.com>
2015-02-21 15:08:30 +0800

    add location setting

--- src/com/yy/appweather/SettingActivity.java
+++ src/com/yy/appweather/SettingActivity.java
@@ -1,18 +1,37 @@
 package com.yy.appweather;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
   
 import android.preference.Preference;  
+import android.preference.PreferenceManager;
 
 
 
 public class SettingActivity extends PreferenceActivity 
 	implements Preference.OnPreferenceChangeListener {
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.pref_general);
+		bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+		
+	}
+	
+	private void bindPreferenceSummaryToValue(Preference preference) {
+		// TODO Auto-generated method stub
+		preference.setOnPreferenceChangeListener(this);
+		
+		onPreferenceChange(preference, 
+				PreferenceManager
+					.getDefaultSharedPreferences(preference.getContext())
+					.getString(preference.getKey(), ""));
+		
+	}
+
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -22,6 +41,17 @@ public class SettingActivity extends PreferenceActivity
 	@Override
 	public boolean onPreferenceChange(Preference preference, Object newValue) {
 		// TODO Auto-generated method stub
-		return false;
+		String stringValue = newValue.toString();
+		
+		if(preference instanceof ListPreference){
+			ListPreference listPreference = (ListPreference) preference;
+			int prefIndex = listPreference.findIndexOfValue(stringValue);
+			if(prefIndex >= 0){
+				preference.setSummary(listPreference.getEntries()[prefIndex]);
+			}
+		}else{
+			preference.setSummary(stringValue);
+		}
+		return true;
 	}
 }
