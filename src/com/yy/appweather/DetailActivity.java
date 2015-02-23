a2d61153fa7d9a872cea20daf08349adbd687c9d
yysc <494792342@qq.com>
2015-02-23 13:08:51 +0800

    map and share not working

--- src/com/yy/appweather/DetailActivity.java
+++ src/com/yy/appweather/DetailActivity.java
@@ -5,8 +5,12 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -48,8 +52,14 @@ public class DetailActivity extends Activity {
 	 * A placeholder fragment containing a simple view.
 	 */
 	public static class PlaceholderFragment extends Fragment {
+		
+		private static final String LOG_TAG = PlaceholderFragment.class.getSimpleName();
+		
+		private static final String FORECAST_SHARE_HASHTAG = "#AppWeather";
+		private String mForecastStr;
 
 		public PlaceholderFragment() {
+			setHasOptionsMenu(true);
 		}
 
 		@Override
@@ -59,11 +69,35 @@ public class DetailActivity extends Activity {
 			View rootView = inflater.inflate(R.layout.fragment_detail,
 					container, false);
 			if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
-				String forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+				mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
 				((TextView ) rootView.findViewById(R.id.detail_text))
-						.setText(forecast);
+						.setText(mForecastStr);
 			}
 			return rootView;
+		}
+		
+		private Intent createShareForecastIntent(){
+			Intent shareIntent = new Intent(Intent.ACTION_SEND);
+			shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+			shareIntent.setType("text/plain");
+			shareIntent.putExtra(Intent.EXTRA_TEXT,
+					mForecastStr + FORECAST_SHARE_HASHTAG);
+			return shareIntent;
+		}
+		
+		@Override
+		public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+			// TODO Auto-generated method stub		
+			inflater.inflate(R.menu.detailfragment, menu);
+			MenuItem menuItem = menu.findItem(R.id.action_share);
+			ShareActionProvider mShareActionProvider = 
+					(ShareActionProvider ) MenuItemCompat.getActionProvider(menuItem);
+			
+			if(mShareActionProvider != null){
+				mShareActionProvider.setShareIntent(createShareForecastIntent());
+			}else {
+				Log.d(LOG_TAG, "Share Action Provider is null");
+			}
 		}
 	}
 }
